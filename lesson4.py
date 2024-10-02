d9c51772ec077118e2238adaa0e65f74ab083a76
LoneBoyNes16 <nestoroawurumibe6@gmail.com>
2024-10-02 12:19:34 +0100

    first commit

---
+++ lesson4.py
@@ -0,0 +1,14 @@
+def nestor_awurumibe_func():
+    # this is my name full name
+    name = "nestor_awuumibe"
+    # this is not my real age
+    age = 20
+    # i am definitely a male
+    is_male =True
+    # i am this tall in height
+    height = 5.8
+    print(name,age,is_male,height)
+
+
+    nestor_awurumibe_func()
+    
